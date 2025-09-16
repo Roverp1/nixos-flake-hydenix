9f9d3fc52f04fad4964394a1e69bcb330d01215f
Roverp <yar.zubaha@proton.me>
2025-09-16 22:17:41 +0200

    feat(system): install important fonts

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -13,6 +13,14 @@
     # pkgs.userPkgs.vscode - your personal nixpkgs version
   ];
 
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+
+    nerd-fonts.jetbrains-mono
+  ];
+
   nix = {
     gc = {
       automatic = true;
