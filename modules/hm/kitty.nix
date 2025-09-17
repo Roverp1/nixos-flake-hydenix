1517bc8b1c92a98ed63fb91ab09866f892f6499a
Roverp <yar.zubaha@proton.me>
2025-09-17 23:44:08 +0200

    feat(kitty): create nix configuration for kitty

---
+++ modules/hm/kitty.nix
@@ -0,0 +1,40 @@
+{...}: {
+  programs.kitty = {
+    enable = true;
+
+    font = {
+      name = "JetBrainsMono NF Regular";
+      size = 14;
+    };
+
+    settings = {
+      confirm_os_window_close = 3;
+      enable_audio_bell = true;
+
+      curesor_shape = "block";
+      mouse_hide_wait = 2.0;
+      cursor_blink_interval = 0;
+
+      background_opacity = 0.9;
+    };
+
+    keybindings = {
+      "ctrl+shift+h" = "no_op";
+      "ctrl+shift+l" = "no_op";
+      "ctrl+alt+h" = "no_op";
+      "ctrl+alt+l" = "no_op";
+      "ctrl+alt+j" = "no_op";
+      "ctrl+alt+k" = "no_op";
+
+      "ctrl+shift+minus" = "no_op";
+      "ctrl+shift+equal" = "no_op";
+
+      "map+shift+period" = "increase_font_size";
+      "map+shift+comma" = "decrease_font_size";
+    };
+
+    extraConfig = ''
+      include theme.conf
+    '';
+  };
+}
