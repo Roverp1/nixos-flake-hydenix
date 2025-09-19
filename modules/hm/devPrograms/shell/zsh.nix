64c9203ef172279289fefde357fe671f84cecbf0
Roverp <yar.zubaha@proton.me>
2025-09-19 21:06:51 +0200

    feat(hm/zsh): setup pure-prompt for zsh

--- modules/hm/devPrograms/shell/zsh.nix
+++ modules/hm/devPrograms/shell/zsh.nix
@@ -7,9 +7,18 @@
   cfg = config.roverp.shell.zsh;
 in {
   options = {
-    roverp.shell.zsh.enable = lib.mkOption {
-      default = true;
-      description = "Enable zsh module";
+    roverp.shell.zsh = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "Enable zsh module";
+      };
+
+      purePrompt.enable = lib.mkOption {
+        type = lib.types.bool;
+        default = cfg.enable && !config.roverp.shell.ohMyPosh.enable;
+        description = "Enable pure prompt for zsh";
+      };
     };
   };
 
@@ -22,12 +31,28 @@ in {
           name = zsh-syntax-highlighting.pname;
           src = zsh-syntax-highlighting.src;
         }
+
+        (lib.mkIf cfg.purePrompt.enable {
+          name = pure-prompt.pname;
+          src = pure-prompt.src;
+        })
       ];
 
       initContent = let
+        purePromptConfig = ''
+          autoload -U promptinit; promptinit
+          prompt pure
+
+          prompt_newline='%666v'
+          PROMPT=" $PROMPT"
+        '';
+
         zshConfig = lib.mkOrder 1000 (builtins.readFile ./../../../../Configs/.config/zsh/.zshrc);
+        zshConfigEarly = lib.mkOrder 500 ''
+          ${purePromptConfig}
+        '';
       in
-        lib.mkMerge [zshConfig];
+        lib.mkMerge [zshConfigEarly zshConfig];
     };
   };
 }
