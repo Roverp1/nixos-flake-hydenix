ddf34fc4775df1c4b0e11bd51ff128b2273bd8e4
Roverp <yar.zubaha@proton.me>
2025-09-19 21:53:57 +0200

    fix(hyprland): -> disable shaders

--- modules/hm/hyprland.nix
+++ modules/hm/hyprland.nix
@@ -1,9 +1,17 @@
 {...}: {
-  hydenix.hm.hyprland.keybindings = {
-    enable = true;
-    extraConfig = ''
-      ${builtins.readFile ./../../Configs/.config/hypr/unbinds.conf}
-      ${builtins.readFile ./../../Configs/.config/hypr/keybinds.conf}
-    '';
+  hydenix.hm.hyprland = {
+    keybindings = {
+      enable = true;
+      extraConfig = ''
+        ${builtins.readFile ./../../Configs/.config/hypr/unbinds.conf}
+        ${builtins.readFile ./../../Configs/.config/hypr/keybinds.conf}
+      '';
+    };
+
+    animations = {
+      preset = "minimal-1";
+    };
+
+    shaders.enable = false;
   };
 }
