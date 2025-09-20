9abb2a327a56b26581ee7340f8021b102a334314
Roverp <yar.zubaha@proton.me>
2025-09-20 17:39:06 +0200

    feat(hm/zsh): configure fzf compeletion menu

--- modules/hm/devPrograms/shell/zsh.nix
+++ modules/hm/devPrograms/shell/zsh.nix
@@ -19,6 +19,12 @@ in {
         default = cfg.enable && !config.roverp.shell.ohMyPosh.enable;
         description = "Enable pure prompt for zsh";
       };
+
+      fzfIntegration.enable = lib.mkOption {
+        type = lib.types.bool;
+        default = config.roverp.programs.fzf.enable;
+        description = "Enable fzf integration for zsh";
+      };
     };
   };
 
@@ -54,10 +60,15 @@ in {
           src = pure-prompt.src;
           file = "pure.zsh";
         })
+
+        (lib.mkIf cfg.fzfIntegration.enable {
+          name = zsh-fzf-tab.pname;
+          src = zsh-fzf-tab.src;
+        })
       ];
 
       initContent = let
-        purePromptConfig = lib.mkOrder 600 ''
+        purePromptConfig = lib.mkIf cfg.purePrompt.enable lib.mkOrder 600 ''
           autoload -U promptinit; promptinit
           prompt pure
 
@@ -68,9 +79,15 @@ in {
           }
         '';
 
+        fzfConfig = lib.mkIf cfg.fzfIntegration.enable lib.mkOrder 1050 ''
+          zstyle ':completion:*' menu no
+          zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+          zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+        '';
+
         zshConfig = lib.mkOrder 1000 (builtins.readFile ./../../../../Configs/.config/zsh/.zshrc);
       in
-        lib.mkMerge [purePromptConfig zshConfig];
+        lib.mkMerge [purePromptConfig zshConfig fzfConfig];
     };
 
     home.file = {
