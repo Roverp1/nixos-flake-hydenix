197090b49ad6bca678ed936ad41a31f365462925
Roverp <yar.zubaha@proton.me>
2025-09-20 14:29:07 +0200

    feat(hm/xdg): disable hydenix's xdg module && replace with custom xdg module based on hydenix.hm.xdg

--- modules/hm/default.nix
+++ modules/hm/default.nix
@@ -8,6 +8,7 @@
     ./devPrograms
 
     ./hyprland.nix
+    ./xdg.nix
     ./neovim.nix
     ./zen-browser.nix
     # ./kitty.nix
@@ -23,30 +24,6 @@
       # Default programs
       EDITOR = "nvim";
       BROWSER = "zen";
-
-      # XDG Base Directories (redundant?)
-      XDG_CONFIG_HOME = "${config.home.homeDirectory}/.config";
-      XDG_DATA_HOME = "${config.home.homeDirectory}/.local/share";
-      XDG_CACHE_HOME = "${config.home.homeDirectory}/.cache";
-
-      ZDOTDIR = "${config.xdg.configHome}/zsh";
-
-      # Development tools
-      CARGO_HOME = "${config.xdg.dataHome}/cargo";
-      GOPATH = "${config.xdg.dataHome}/go";
-      GOBIN = "${config.xdg.dataHome}/go/bin";
-      GOMODCACHE = "${config.xdg.cacheHome}/go/mod";
-      NPM_CONFIG_USERCONFIG = "${config.xdg.configHome}/npm/npmrc";
-
-      # Other programs
-      LESSHISTFILE = lib.mkDefault "${config.xdg.cacheHome}/less_history";
-      PYTHON_HISTORY = "${config.xdg.dataHome}/python/history";
-      WGETRC = "${config.xdg.configHome}/wget/wgetrc";
-      GNUPGHOME = "${config.xdg.dataHome}/gnupg";
-
-      # FZF configuration
-      FZF_DEFAULT_OPTS = "--style minimal --color 16 --layout=reverse --height
-30%";
     };
   };
 
@@ -76,6 +53,7 @@
     terminals.enable = false;
     terminals.kitty.enable = false;
     # shell.enable = false;
+    xdg.enable = false;
 
     theme = {
       active = "Another World";
