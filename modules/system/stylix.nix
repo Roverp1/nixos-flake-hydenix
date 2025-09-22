e64bdfbad38bb38ea34c943818408d781fbe8654
Roverp <yar.zubaha@proton.me>
2025-09-23 01:16:37 +0200

    fix(stylix): -> simplify configuration to fix stylix

--- modules/system/stylix.nix
+++ modules/system/stylix.nix
@@ -1,78 +1,27 @@
 {
-  config,
   lib,
   pkgs,
   ...
 }: let
-  inherit (lib) mkOption types optionalAttrs;
+  inherit (lib) optionalAttrs;
 
-  cfg = config.roverp.stylix;
+  selectTheme = "oxocarbon-dark";
+
+  themes = import ./themes/default.nix {inherit pkgs;};
+  theme = themes.${selectTheme};
 in {
-  options.roverp.stylix = {
-    enable = lib.mkOption {
-      type = lib.types.bool;
-      default = !config.hydenix.hm.theme.enable;
-      description = "Enable stylix theming";
-    };
+  stylix = {
+    enable = true;
 
-    activeTheme = lib.mkOption {
-      type = types.enum ["oxocarbon-dark"];
-      default = "oxocarbon-dark";
-      description = "Active theme name";
-    };
+    base16Scheme = theme.base16Scheme;
+    image = theme.wallpaper;
+    # polarity = theme.polarity;
 
-    themes = mkOption {
-      type = types.attrsOf (types.submodule {
-        options = {
-          base16Scheme = mkOption {
-            type = types.nullOr (types.either types.path types.attrs);
-            default = null;
-            description = "Base16 color scheme";
-          };
-
-          wallpaper = mkOption {
-            type = types.nullOr types.path;
-            default = null;
-            description = "Wallpaper path";
-          };
-
-          polarity = mkOption {
-            type = types.nullOr (types.enum ["dark" "light"]);
-            default = null;
-            description = "Theme polarity for auto-generation";
-          };
-        };
-      });
-
-      default = {};
-      description = "Available themes";
+    fonts = {
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrainsMono NF Regular";
+      };
     };
   };
-
-  config = lib.mkIf cfg.enable (
-    let
-      activeTheme = cfg.themes.${cfg.activeTheme};
-    in {
-      stylix =
-        {
-          enable = true;
-
-          fonts = {
-            monospace = {
-              package = pkgs.nerd-fonts.jetbrains-mono;
-              name = "JetBrainsMono NF Regular";
-            };
-          };
-        }
-        // (optionalAttrs (activeTheme.base16Scheme != null) {
-          base16Scheme = activeTheme.base16Scheme;
-        })
-        // (optionalAttrs (activeTheme.wallpaper != null) {
-          image = activeTheme.wallpaper;
-        })
-        // (optionalAttrs (activeTheme.polarity != null) {
-          polarity = activeTheme.polarity;
-        });
-    }
-  );
 }
--- modules/system/themes/default.nix
+++ modules/system/themes/default.nix
@@ -1,8 +1,6 @@
 {pkgs, ...}: {
-  roverp.stylix.themes = {
-    oxocarbon-dark = {
-      base16Scheme = "${pkgs.base16-schemes}/share/themes/oxocarbon-dark.yaml";
-      wallpaper = ./wallpapers/apcolyplse-neon-forest.png;
-    };
+  oxocarbon-dark = {
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/oxocarbon-dark.yaml";
+    wallpaper = ./wallpapers/apcolyplse-neon-forest.png;
   };
 }
